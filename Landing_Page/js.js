a5d404cb7849e0aae4b59ab2b68a9eb4e0656f28
giovana.borges <gigi12345as17@gmail.com>
2023-02-09 18:49:50 -0300

    :feat change mode

---
+++ Landing_Page/js.js
@@ -0,0 +1,66 @@
+let root = document.querySelector(':root');
+let imgSun = "https://img.icons8.com/ios-filled/3/null/smiling-sun.png"
+let imgMoon = "https://img.icons8.com/glyph-neue/34/null/bright-moon.png"
+
+function lightMode(){
+   
+    root.style.setProperty('--bg-white', light.bg);
+    root.style.setProperty('--bg-zinc-900',light.text);
+    root.style.setProperty('--bg-zinc-200', light.bgCard);
+
+    setLocalStorageLight()
+
+    document.getElementById("modeLight").style.display="block"
+    document.getElementById("modeDark").style.display="none"
+
+   
+}
+
+function darkMode(){
+
+   
+    root.style.setProperty('--bg-white',dark.bg);
+    root.style.setProperty('--bg-zinc-900',dark.text);
+    root.style.setProperty('--bg-zinc-200', dark.bgCard);
+
+    setLocalStorageDark()
+
+    document.getElementById("modeDark").style.display="block"
+    document.getElementById("modeLight").style.display="none"
+
+  
+}
+
+
+// save info on local storage
+
+function setLocalStorageDark(){
+    localStorage.setItem("bg",dark.bg);
+    localStorage.setItem("text",dark.text)
+    localStorage.setItem("card",dark.bgCard)
+}
+
+function setLocalStorageLight(){
+    localStorage.setItem("bg",light.bg);
+    localStorage.setItem("text",light.text)
+    localStorage.setItem("card",light.bgCard)
+}
+
+
+const dark = {
+    bg: "#1C1917",
+    text: "white",
+    bgCard:"#1C1917"
+}
+
+const light = {
+    bg : "#F2F2F2",
+    text: "#1C1917",
+    bgCard:"#E4E4E7"
+}
+
+window.addEventListener("load", (event) => {
+    root.style.setProperty('--bg-white',localStorage.getItem("bg")?localStorage.getItem("bg"): light.bg);
+    root.style.setProperty('--bg-zinc-900',localStorage.getItem("text")?localStorage.getItem("text"):light.text);
+    root.style.setProperty('--bg-zinc-200',localStorage.getItem("card")?localStorage.getItem("card"): light.bgCard);
+  });
